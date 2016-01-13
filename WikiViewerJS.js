fa2e938f2bfd834167bedee11af5b66940c9a827
FrankCardillo <facardillo91@gmail.com>
2016-01-13 07:12:27 -0800

    Source files for project

---
+++ WikiViewerJS.js
@@ -0,0 +1,22 @@
+var app = angular.module('WikiViewer', []);
+app.controller('MainCtrl', function($scope, $http, $timeout) {
+  
+  $scope.results = [];
+  
+  $("#search").click(function() {
+    var title = $("#query").val();
+    var api = 'http://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=';
+    var cb = '&callback=JSON_CALLBACK';
+    var page = 'http://en.wikipedia.org/?curid=';
+    
+    $http.jsonp(api + title + cb).success(function(data) {
+      var results = data.query.pages;
+      angular.forEach(results, function(v,k)  {
+        $scope.results.push({title: v.title, body: v.extract, page: page + v.pageid})
+      })
+    });
+    $("#query").val('');
+    $scope.results = [];
+  });
+  
+});
